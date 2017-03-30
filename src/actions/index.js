f4d78ad6576b2fa979d7975313a570edd2e6ff75
arjunkhode <arjunkhode@gmail.com>
2017-03-29 23:20:24 -0700

    Got the searchbar to show input

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
+
+const API_KEY = '546434454b71b281b3be15ddf7fb0574';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+	return{
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
